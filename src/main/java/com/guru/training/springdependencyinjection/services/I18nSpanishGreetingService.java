a23905143dd972a2aeff7947dfa5cade75fd36b8
grjus <grjus@pm.me>
2021-08-11 12:42:05 +0200

    Added default profile option

--- src/main/java/com/guru/training/springdependencyinjection/services/I18nSpanishGreetingService.java
+++ src/main/java/com/guru/training/springdependencyinjection/services/I18nSpanishGreetingService.java
@@ -2,7 +2,7 @@ package com.guru.training.springdependencyinjection.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("SP")
+@Profile({"SP", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
