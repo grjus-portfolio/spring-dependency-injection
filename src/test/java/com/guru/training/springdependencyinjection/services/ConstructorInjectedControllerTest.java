b44b7326f476341e6d1c74fb7e2ae12e07daf5af
grjus <grjus@pm.me>
2021-08-11 10:33:09 +0200

    Added unit tests for dependency injection

--- src/test/java/com/guru/training/springdependencyinjection/services/ConstructorInjectedControllerTest.java
+++ src/test/java/com/guru/training/springdependencyinjection/services/ConstructorInjectedControllerTest.java
@@ -1,4 +1,23 @@
+package com.guru.training.springdependencyinjection.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConstructorInjectedControllerTest {
-  
+
+    ConstructorInjectedController controller;
+
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController( new GreetingServiceImpl());
+    }
+    @Test
+    void name() {
+        System.out.println(controller.getGreeting());
+    }
+
+
 }
--- src/test/java/com/guru/training/springdependencyinjection/services/SetterInjectedControllerTest.java
+++ src/test/java/com/guru/training/springdependencyinjection/services/SetterInjectedControllerTest.java
@@ -1,4 +1,22 @@
+package com.guru.training.springdependencyinjection.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SetterInjectedControllerTest {
-  
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void name() {
+        System.out.println(controller.getGreeting());
+    }
 }
